26ecb29c0f2abcfa6167c687c8f097407b6ca0e7
NodeKnight <josueruiztorres100@gmail.com>
2023-09-19 15:15:47 -0600

    added initial files

---
+++ Scrips/new_game.gd
@@ -0,0 +1,21 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$ObjetoAlmacenador/Protagonist.play("Spin")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_iniciar_pressed():
+	var name = $ObjetoAlmacenador2/TxtName.text
+	var FavThing = $ObjetoAlmacenador2/TxtFavThing.text
+
+
+func _on_regresar_pressed():
+			get_tree().change_scene_to_file("res://Escenas/opcionesTitulo.tscn")
